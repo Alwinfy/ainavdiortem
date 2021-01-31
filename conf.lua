2140a00e5086cb1c5fa5443d00f8683c1a087749
NateTheGreat233 <nateguntvedt@gmail.com>
2021-01-30 16:02:16 -0800

    Basic Movement
    Use arrow keys to move the temporary player which collides with the 
    ground. Jumping needs to be fixed though, the player can essentially 
    hold up and fly.

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+	t.title = "Game Jam"
+	t.version = "11.3"
+	t.window.width = 1400
+	t.window.height = 1000
+
+	t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,46 @@
+function love.load()
+  meterSize = 64
+  love.physics.setMeter(meterSize) --height of 1 meter in pixels for physics purposes
+  world = love.physics.newWorld(0, 9.81*meterSize, true)
+
+  ground = {}
+  ground.body = love.physics.newBody(world, 700, 900, "static")
+  ground.shape = love.physics.newRectangleShape(1400, 200)
+  ground.fixture = love.physics.newFixture(ground.body, ground.shape)
+
+  player = {}
+  player.body = love.physics.newBody(world, 500, 500, "dynamic")
+  player.shape = love.physics.newRectangleShape(100, 100)
+  player.fixture = love.physics.newFixture(player.body, player.shape)
+end
+
+function love.update(dt)
+  world:update(dt)
+
+  x, y = player.body:getLinearVelocity()
+  if love.keyboard.isDown("right") or love.keyboard.isDown("left") then
+    if love.keyboard.isDown("right") then
+      player.body:setLinearVelocity(400, y)
+    end
+    if love.keyboard.isDown("left") then
+      player.body:setLinearVelocity(-400, y)
+    end
+  else
+    player.body:setLinearVelocity(0, y)
+  end
+
+  if love.keyboard.isDown("up") then
+    player.body:applyForce(0, -10000)
+  end
+
+end
+
+function love.draw()
+  love.graphics.setColor(0, 255, 0)
+  love.graphics.polygon("fill", ground.body:getWorldPoints(
+                         ground.shape:getPoints()))
+
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.polygon("fill", player.body:getWorldPoints(
+                         player.shape:getPoints()))
+end
