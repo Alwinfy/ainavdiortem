572fa165712332d844e4b65bd970f32c09ab7a32
Gabriel Rodríguez <gabrc52@gmail.com>
2021-02-04 15:41:29 -0600

    make pause screen fancier

--- states/pause.lua
+++ states/pause.lua
@@ -1,7 +1,20 @@
 local pause = {}
 
+-- Adapted from the example in the documentation (https://hump.readthedocs.io/en/latest/gamestate.html) --
+
+function pause:enter(from)
+    self.from = from
+end
+
 function pause:draw()
-    love.graphics.print('There is no pause screen.', 10, 10)
+    local W, H = love.graphics.getWidth(), love.graphics.getHeight()
+    -- draw previous screen
+    self.from:draw()
+    -- overlay with pause message
+    love.graphics.setColor(0, 0, 0, 0.66)
+    love.graphics.rectangle('fill', 0,0, W, H)
+    love.graphics.setColor(255,255,255)
+    love.graphics.printf('PAUSED\nPress Esc to resume', 0, H/2, W, 'center')
 end
 
 return pause
