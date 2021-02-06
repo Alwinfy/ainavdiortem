5b8249bf8438c40c5f94fca17354f245bd1edd30
Gabriel Rodríguez <gabrc52@gmail.com>
2021-02-06 17:01:59 -0600

    udpate spikes tilemap path

--- assets/tiled/spikes.tsx
+++ assets/tiled/spikes.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="spikes" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="spikes_tilemap.png" width="48" height="48"/>
+ <image source="../tiles/spikes_tilemap.png" width="48" height="48"/>
 </tileset>
