5b82d4d5fda92d7d7d87d0cb2f7229dde6b5280b
Gabriel Rodríguez <gabrc52@gmail.com>
2021-02-06 14:07:08 -0600

    update tiled tileset

--- assets/tiled/textures.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="textures" tilewidth="16" tileheight="16" tilecount="92" columns="4">
- <image source="textures.png" width="64" height="383"/>
-</tileset>
